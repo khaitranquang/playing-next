6ecdaf0c7f2ea372667b6b63378c6c022bf1d198
Tran Quang Khai <khai.tranquang.8797@gmail.com>
2020-05-05 22:46:59 +0700

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
+import PropTypes from "prop-types";
+
 import Head from 'next/head';
 import Button from '@material-ui/core/Button';
 
-const Index = () => (
+const Index = ({user}) => (
     <div style={{ padding: '10px 45px' }}>
         <Head>
             <title>Index page</title>
             <meta name="description" content="This is a description of the Index page" />
         </Head>
         <p>Content on Index page</p>
-        <Button variant="contained">MUI button</Button>
+        <p>Email: {user.email}</p>
     </div>
 );
+
+Index.getInitialProps = async (ctx) => ({user: ctx.query.user});
+
+Index.propTypes = {
+    user: PropTypes.shape({
+        email: PropTypes.string.isRequired,
+    }),
+};
+
+Index.defaultProps = {
+    user: null
+};
 
 export default Index;
